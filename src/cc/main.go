d3fd2f650d070073d5283ad4862b96cd514d3c74
andrew chambers <andrewchamberss@gmail.com>
2014-06-13 20:02:11 +1200

    missing file from commit

--- src/cc/main.go
+++ src/cc/main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
-import "flag"
+import ( 
+	"cc/cpp"
+	"fmt"
+	"flag"
+	"io"
+	"os"
+ )
 
 func printVersion() {
 	fmt.Println("cc version 0.01")
@@ -19,21 +24,49 @@ func printUsage() {
 	flag.PrintDefaults()
 }
 
+func preprocessFile(sourceFile string, out io.Writer) {
+	cpp.
+}
+
 func main() {
+	flag.Usage = printUsage
 	preprocessOnly := flag.Bool("E", false, "Preprocess only")
 	version := flag.Bool("version", false, "Print version info and exit.")
-	help := flag.Bool("h", false, "Print help message and exit.")
-	outputFile := flag.String("o", "-", "File to write output to, - for stdout.")
+	outputPath := flag.String("o", "-", "File to write output to, - for stdout.")
 	flag.Parse()
-
-	if *help {
-		printUsage()
-		return
-	}
 
 	if *version {
 		printVersion()
+		return
 	}
 
-	fmt.Println("Arguments: ", *preprocessOnly, *outputFile)
+	if flag.NArg() == 0 {
+		printUsage()
+		os.Exit(1)
+	}
+
+	if flag.NArg() != 1 {
+		fmt.Println("Bad number of args, please specify a single source file.")
+		os.Exit(1)
+	}
+
+	input := flag.Args()[0]
+	var output io.Writer
+	var err error
+
+	if *outputPath == "-" {
+		output = os.Stdout
+	} else {
+		output, err = os.Create(*outputPath)
+		if err != nil {
+			fmt.Errorf("Failed to open output file %s", err)
+			os.Exit(1)
+		}
+	}
+
+	if *preprocessOnly {
+		preprocessOnly
+	} else {
+		CompileFile(input, nil, output)
+	}
 }
