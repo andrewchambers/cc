b4441fb210b5a853175865f816475f3eb906998c
andrew chambers <andrewchamberss@gmail.com>
2014-07-09 00:18:55 +1200

    missing macro

---
+++ src/cc/cpp/macro.go
@@ -0,0 +1,31 @@
+package cpp
+
+type objMacro struct {
+	tokens *tokenList
+}
+
+func newObjMacro(tokens *tokenList) *objMacro {
+	return &objMacro{tokens}
+}
+
+type funcMacro struct {
+	//Map of macro string to arg position
+	//0 based indexe
+	args map[string]int
+	//Tokens of the macro.
+	tokens *tokenList
+}
+
+//args should be a list of ident tokens
+func newFuncMacro(args *tokenList, tokens *tokenList) *funcMacro {
+	ret := new(funcMacro)
+	ret.args = make(map[string]int)
+	idx := 0
+	for e := args.front(); e != nil; e = e.Next() {
+		tok := e.Value.(*Token)
+		ret.args[tok.Val] = idx
+		idx += 1
+	}
+	ret.tokens = tokens
+	return ret
+}
